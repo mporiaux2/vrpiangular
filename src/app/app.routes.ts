a38f8f3f0c5e844d2104013d7164605a6d2bb356
michel <michel.poriaux@skynet.be>
2026-01-21 11:25:46 +0100

    premier commit: menu général

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import {AdminComponent} from './components/admin/admin.component';
+import {CycloComponent} from './components/cyclo/cyclo.component';
+import {HomeComponent} from './components/home/home.component';
+import {GestionparcoursComponent} from './components/gestionparcours/gestionparcours.component';
+import {GestionpointsComponent} from './components/gestionpoints/gestionpoints.component';
+import {SimulationreelComponent} from './components/simulationreel/simulationreel.component';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'admin', component: AdminComponent},
+  {path: 'cyclo', component: CycloComponent},
+  {path: '', component: HomeComponent},
+  {path: 'gestionparcours', component:GestionparcoursComponent},
+  {path: 'gestionpoints', component:GestionpointsComponent},
+  {path: 'simulationreel', component:SimulationreelComponent},
+
+];
