45c69de61cefd75d9a02f1a0de74c357b408be52
michel <michel.poriaux@skynet.be>
2026-01-23 10:07:50 +0100

    création des différents écrans vides

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import {HomeComponent} from './components/home/home.component';
 import {GestionparcoursComponent} from './components/gestionparcours/gestionparcours.component';
 import {GestionpointsComponent} from './components/gestionpoints/gestionpoints.component';
 import {SimulationreelComponent} from './components/simulationreel/simulationreel.component';
+import {ChoixparcoursComponent} from './components/choixparcours/choixparcours.component';
+import {ChoixpointComponent} from './components/choixpoint/choixpoint.component';
+import {DemarrerComponent} from './components/demarrer/demarrer.component';
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
@@ -13,5 +16,8 @@ export const routes: Routes = [
   {path: 'gestionparcours', component:GestionparcoursComponent},
   {path: 'gestionpoints', component:GestionpointsComponent},
   {path: 'simulationreel', component:SimulationreelComponent},
+  {path: 'choixparcours', component:ChoixparcoursComponent},
+  {path: 'choixpoint', component:ChoixpointComponent},
+  {path: 'demarrer', component:DemarrerComponent},
 
 ];
