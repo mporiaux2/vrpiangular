b500528a62004a06aa1c41073a8dbb5937c7807e
michel <michel.poriaux@skynet.be>
2026-01-30 10:23:32 +0100

    gestion du zomm et de la rotation

--- src/app/components/demarrer/demarrer.component.ts
+++ src/app/components/demarrer/demarrer.component.ts
@@ -27,7 +27,7 @@ async function sleep(ms: number) {
 
 export class DemarrerComponent implements  OnInit,OnDestroy {
   private googleMapsScriptLoaded = false;
-  vitZoom:number= 0.90;
+  vitZoom:number= 2;
   FADE_MS = 1500;
   head = 30;
   vit = 10;
@@ -38,6 +38,23 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
   MAX_WAIT_MS = 4000;
   panoA:any;
   panoB:any;
+  wstacx:WebSocket | undefined;
+  wsarduino: WebSocket | undefined;
+
+  zoomact:number=1;
+
+  startzoom: number | undefined=0;
+
+  adist: number =0;
+
+  dtot:number=0;
+
+  dist:number=0;
+  rot:number=0;
+
+  active:any;
+  next:any;
+
 
   constructor(private zone: NgZone) {
   }
@@ -50,7 +67,44 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
 
     this.loadGoogleMapsScript();
     this.setFadeDuration(this.FADE_MS);
+    this.wstacx = new WebSocket("ws://pi5.local/wstacxhtml");
+//wstacx.onopen = () => wstacx.send("activation de wstacxhtml");
+    this.wstacx.onmessage = (e) => {
+      console.log("réception vitesse : " + e.data);
+      this.vit = parseFloat(e.data);
+    }
 
+    this.wsarduino = new WebSocket("ws://pi5.local/wsarduinohtml");
+//wsarduino.onopen = () => wsarduino.send("activation de wsarduinohtml");
+    this.wsarduino.onmessage = (e) => {
+      console.log("réception direction :" + e.data);
+      let direction: number = parseInt(e.data);
+      switch (direction) {
+        case 0 :
+
+          break;
+        case 1 :
+          this.rot -= 3;
+          this.rotate();
+          break;
+        case 2 :
+          this.rot += 3;
+          this.rotate();
+          break;
+      }
+    }
+
+
+  }
+  rotate() {
+    this.active.setPov({
+        heading: this.rot,
+        pitch: 0,
+      });
+    this.next.setPov({
+      heading:this.rot,
+      pitch:0,
+    })
   }
 
   ngOnDestroy(): void {
@@ -89,23 +143,17 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
 
 
 
+
+
   private initializeStreetView(): void {
     console.log('Initialisation Street View (Angular)');
 
-    const panorama = new (window as any).google.maps.StreetViewPanorama(
-      document.getElementById('streetview') as HTMLElement,
-      {
-        position: {lat: 50.8503, lng: 4.3517}, // Bruxelles par ex
-        pov: {heading: 165, pitch: 0},
-        zoom: 1,
-      }
-    );
 
     this.panoA = new (window as any).google.maps.StreetViewPanorama(
       document.getElementById('panoA') as HTMLElement,
       {
-        position: {lat: 50.8503, lng: 4.3517}, // Bruxelles par ex
-        pov: {heading: 165, pitch: 0},
+        position: {lat: 44.442500, lng: 4.413828}, // lagorce
+        pov: {heading: 30, pitch: 0},
         zoom: 1,
       }
     );
@@ -114,8 +162,8 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
     this.panoB = new (window as any).google.maps.StreetViewPanorama(
       document.getElementById('panoB') as HTMLElement,
       {
-        position: {lat: 50.8503, lng: 4.3517}, // Bruxelles par ex
-        pov: {heading: 165, pitch: 0},
+        position: {lat: 44.442500, lng: 4.413828}, // lagorce
+        pov: {heading: 30, pitch: 0},
         zoom: 1,
       }
     );
@@ -193,31 +241,35 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
   }
 
   async   avancer() {
-    let curr;
+
     do {
       console.log("ok0");
       if (this.isTransitioning) return;
       this.isTransitioning = true;
 
-      const active = (this.current === "A") ? this.panoA : this.panoB;
-      const next = (this.current === "A") ? this.panoB : this.panoA;
+      this.active = (this.current === "A") ? this.panoA : this.panoB;
+      this.next = (this.current === "A") ? this.panoB : this.panoA;
 
-      const links = active.getLinks();
+
+
+
+
+      const links = this.active.getLinks();
       if (!links || links.length === 0) {
         this.isTransitioning = false;
         return;
       }
-      console.log(" nbre liens = " + active.getLinks().length);
-      console.log(" heading active= " + active.pov.heading);
-      this.curr = active.links[0];
-      let differ = this.difference(active, this.curr);
+      console.log(" nbre liens = " + this.active.getLinks().length);
+      console.log(" heading active= " + this.active.pov.heading);
+      this.curr = this.active.links[0];
+      let differ = this.difference(this.active, this.curr);
       console.log("differ 0 = " + differ);
-      for (let i = 1; i < active.getLinks().length; i++) {
-        differ = this.difference(active, active.links[i]);
+      for (let i = 1; i < this.active.getLinks().length; i++) {
+        differ = this.difference(this.active, this.active.links[i]);
 
         console.log("differ " + i + " = " + differ);
-        if (this.difference(active, this.curr) > this.difference(active, active.links[i])) {
-          curr = active.links[i];
+        if (this.difference(this.active, this.curr) > this.difference(this.active, this.active.links[i])) {
+          this.curr = this.active.links[i];
           console.log("ok =" + i);
         }
       }
@@ -225,20 +277,34 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
       const target :any = this.curr;
 
       // Aligne POV pour que le fondu paraisse “continu”
-      const pov = active.getPov();
+      const pov = this.active.getPov();
+      this.head=pov.heading;
       //next.setPov({ heading: target.heading, pitch: pov.pitch });
       console.log("head = " + this.head);
-      next.setPov({heading: this.head, pitch: pov.pitch});
+      this.next.setPov({heading: this.head, pitch: pov.pitch});
       //next.setZoom(active.getZoom());
-      next.setZoom(1);
+      this.next.setZoom(1);
 
       // Précharge le pano suivant (EN RESTANT INVISIBLE)
-      next.setPano(target.pano);
-      let lapsTemps = 10 / this.vit * 3600;
-      await this.zoomOut(lapsTemps, active);
+      this.next.setPano(target.pano);
+     // let lapsTemps = 10 / this.vit * 3600;
+     // await this.zoomOut(lapsTemps, active);
+
+      const R = 6371000.0;
+      const oldlatr =  this.active.getPosition().lat() * Math.PI / 180;
+      const oldlongr = this.active.getPosition().lng() * Math.PI / 180;
+      const latr = this.next.getPosition().lat() * Math.PI / 180;
+      const longr = this.next.getPosition().lng() * Math.PI / 180;
+
+      this.dist = R * Math.acos(Math.cos(oldlatr) * Math.cos(latr) *
+        Math.cos(longr - oldlongr) + Math.sin(oldlatr) *
+        Math.sin(latr));
+
+      console.log("oldatr="+oldlatr+"oldlongr="+oldlongr+"latr="+latr+"longr="+longr+"distance = "+this.dist);
+      await this.zoomOut(this.active);
 
       // Attendre qu’il soit “prêt” (au moins un pano_changed + petite marge)
-      await this.waitForNextPanoReady(next, target.pano);
+      await this.waitForNextPanoReady(this.next, target.pano);
       // Crossfade SANS NOIR : A reste visible pendant que B monte
       if (this.current === "A") {
         this.crossfade("panoB", "panoA");
@@ -255,22 +321,62 @@ export class DemarrerComponent implements  OnInit,OnDestroy {
   }
 
 
-  async   zoomOut(duration = 2000, pano :any) {
+ async   zoomOut( pano :any) {
 
+    this.zoomact=1;
+    this.adist=0;
+    this.dtot=0;
+    console.log("distance = "+this.dist);
     return new Promise<void>(resolve => {
-      const start = performance.now();
+      this.startzoom = performance.now();
 
       const step = (now: any) => {
-        const progress = Math.min((now - start) / duration, 1);
-        pano.setZoom(1 + progress * this.vitZoom); // zoom vers 3
+        let msg="adist :"+this.adist;
+        msg+="startzoom :"+this.startzoom;
+        msg+="now ="+now;
+        msg+="vitesse :"+this.vit;
+        let lapsTemps = (this.dist -this.adist) / this.vit * 3600;
+        msg+="lapsTemps :"+lapsTemps;
+        // @ts-ignore
+         this.dtot = this.dtot+(now - this.startzoom) / lapsTemps;
+        msg+="dtot :"+this.dtot;
+        this.zoomact= 1+ this.adist/this.dist * this.vitZoom;
+        msg+="zoomact :"+this.zoomact;
+        console.log(msg);
+        // @ts-ignore
 
-        if (progress < 1) requestAnimationFrame(step);
+        this.adist=this.adist+(now-this.startzoom)*this.vit/3600;
+        console.log("adist :"+this.adist);
+        pano.setZoom(this.zoomact); // zoom vers 3
+        this.startzoom = now;
+        if (this.adist< this.dist) requestAnimationFrame(step);
         else resolve();
       }
 
       requestAnimationFrame(step);
     });
   }
+
+  /*
+ async   zoomOut(pano :any) {
+    let duration =  10 / this.vit * 3600;
+    console.log("duration = "+duration);
+    return new Promise<void>(resolve => {
+      const start = performance.now();
+
+      const step = (now: any) => {
+        const dtot = Math.min((now - start) / duration, 1);
+        console.log(("vitzoom = "+this.vitZoom+" dtot ="+dtot));
+        this.zoomact=1+dtot * this.vitZoom;
+        pano.setZoom(this.zoomact); // zoom vers 3
+
+        if (dtot < 1) requestAnimationFrame(step);
+        else resolve();
+      }
+
+      requestAnimationFrame(step);
+    });
+  }*/
 }
 
 
